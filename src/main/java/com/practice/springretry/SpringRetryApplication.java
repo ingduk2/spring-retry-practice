211caf6d461ac2347102094f45d74760a61d5c6e
ingduk2 <ingduk2@naver.com>
2022-05-23 02:32:53 +0900

    modify:EnableConfig move - RetryConfig Configuration Class create

--- src/main/java/com/practice/springretry/SpringRetryApplication.java
+++ src/main/java/com/practice/springretry/SpringRetryApplication.java
@@ -2,9 +2,7 @@ package com.practice.springretry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @SpringBootApplication
 public class SpringRetryApplication {
 
