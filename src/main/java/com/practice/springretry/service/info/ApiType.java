ea78b2341af85744d16959bc1ef8d3496f8b3112
ingduk2 <ingduk2@naver.com>
2022-05-24 09:51:32 +0900

    modify: ApiType add - V3 RetryTemplate - V4 RetryTemplate

--- src/main/java/com/practice/springretry/service/info/ApiType.java
+++ src/main/java/com/practice/springretry/service/info/ApiType.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ApiType {
     V1("V1", "Retry(Annotation), Success"),
-    V2("V2", "Retry(Annotation) Finally Fail, Recover");
+    V2("V2", "Retry(Annotation) Finally Fail, Recover"),
+    V3("V3", "Retry(Template) Success And Recover"),
+    V4("V4", "Retry(Template-Method) Success And Recover");
 
     private final String version;
     private final String description;
